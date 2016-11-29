9efff2bc00eec24452241c9accd0cdd3c86b1da2
Peter Kung <peterkoug@gmail.com>
2016-11-29 19:31:19 +0800

    Coding style cleanup
    Signed-off-by: Peter Kung <peterkoug@gmail.com>

--- Demo/main.c
+++ Demo/main.c
@@ -14,15 +14,12 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-
 /**
  * @file
  * A simple demo application.
  *
  * @author Jernej Kovacic
  */
-
-
 #include <stddef.h>
 
 #include <FreeRTOS.h>
@@ -31,7 +28,6 @@ limitations under the License.
 #include "app_config.h"
 #include "print.h"
 #include "receive.h"
-
 
 /*
  * This diagnostic pragma will suppress the -Wmain warning,
@@ -43,94 +39,89 @@ limitations under the License.
  */
 #pragma GCC diagnostic ignored "-Wmain"
 
-
 /* Struct with settings for each task */
 typedef struct _paramStruct
 {
-    portCHAR* text;                  /* text to be printed by the task */
-    UBaseType_t  delay;              /* delay in milliseconds */
+	portCHAR* text;                  /* text to be printed by the task */
+	UBaseType_t  delay;              /* delay in milliseconds */
 } paramStruct;
 
 /* Default parameters if no parameter struct is available */
 static const portCHAR defaultText[] = "<NO TEXT>\r\n";
 static const UBaseType_t defaultDelay = 1000;
 
-
 /* Task function - may be instantiated in multiple tasks */
 void vTaskFunction( void *pvParameters )
 {
-    const portCHAR* taskName;
-    UBaseType_t  delay;
-    paramStruct* params = (paramStruct*) pvParameters;
-    int i = 0;
+	const portCHAR* taskName;
+	UBaseType_t  delay;
+	paramStruct* params = (paramStruct*) pvParameters;
+	int i = 0;
 
-    taskName = ( NULL==params || NULL==params->text ? defaultText : params->text );
-    delay = ( NULL==params ? defaultDelay : params->delay);
+	taskName = ( NULL==params || NULL==params->text ? defaultText : params->text );
+	delay = ( NULL==params ? defaultDelay : params->delay);
 
-    for( ; ; )
-    {
-        /* Print out the name of this task. */
+	for( ; ; ) {
+		/* Print out the name of this task. */
 
-        //vPrintMsg(taskName);
-	print_msg("%s i = %d\n", taskName, i++);
+		//vPrintMsg(taskName);
+		print_msg("%s i = %d\n", taskName, i++);
 
-        vTaskDelay( delay / portTICK_RATE_MS );
-    }
+		vTaskDelay( delay / portTICK_RATE_MS );
+	}
 
-    /*
-     * If the task implementation ever manages to break out of the
-     * infinite loop above, it must be deleted before reaching the
-     * end of the function!
-     */
-    vTaskDelete(NULL);
+	/*
+	 * If the task implementation ever manages to break out of the
+	 * infinite loop above, it must be deleted before reaching the
+	 * end of the function!
+	 */
+	vTaskDelete(NULL);
 }
-
 
 /* Fixed frequency periodic task function - may be instantiated in multiple tasks */
 void vPeriodicTaskFunction(void* pvParameters)
 {
-    const portCHAR* taskName;
-    UBaseType_t delay;
-    paramStruct* params = (paramStruct*) pvParameters;
-    TickType_t lastWakeTime;
+	const portCHAR* taskName;
+	UBaseType_t delay;
+	paramStruct* params = (paramStruct*) pvParameters;
+	TickType_t lastWakeTime;
 
-    taskName = ( NULL==params || NULL==params->text ? defaultText : params->text );
-    delay = ( NULL==params ? defaultDelay : params->delay);
+	taskName = ( NULL==params || NULL==params->text ? defaultText : params->text );
+	delay = ( NULL==params ? defaultDelay : params->delay);
 
-    /*
-     * This variable must be initialized once.
-     * Then it will be updated automatically by vTaskDelayUntil().
-     */
-    lastWakeTime = xTaskGetTickCount();
+	/*
+	 * This variable must be initialized once.
+	 * Then it will be updated automatically by vTaskDelayUntil().
+	 */
+	lastWakeTime = xTaskGetTickCount();
 
-    for( ; ; )
-    {
-        /* Print out the name of this task. */
+	for( ; ; ) {
+		/* Print out the name of this task. */
 
-        vPrintMsg(taskName);
+		vPrintMsg(taskName);
 
-        /*
-         * The task will unblock exactly after 'delay' milliseconds (actually
-         * after the appropriate number of ticks), relative from the moment
-         * it was last unblocked.
-         */
-        vTaskDelayUntil( &lastWakeTime, delay / portTICK_RATE_MS );
-    }
+		/*
+		 * The task will unblock exactly after 'delay' milliseconds (actually
+		 * after the appropriate number of ticks), relative from the moment
+		 * it was last unblocked.
+		 */
+		vTaskDelayUntil( &lastWakeTime, delay / portTICK_RATE_MS );
+	}
 
-    /*
-     * If the task implementation ever manages to break out of the
-     * infinite loop above, it must be deleted before reaching the
-     * end of the function!
-     */
-    vTaskDelete(NULL);
+	/*
+	 * If the task implementation ever manages to break out of the
+	 * infinite loop above, it must be deleted before reaching the
+	 * end of the function!
+	 */
+	vTaskDelete(NULL);
 }
 
 
 /* Parameters for two tasks */
 static const paramStruct tParam[2] =
 {
-    (paramStruct) { .text="Task1", .delay=2000 },
-    (paramStruct) { .text="Periodic task\r\n", .delay=3000 }
+		(paramStruct) { .text="Task1", .delay=2000 },
+		(paramStruct) { .text="Periodic task\r\n", .delay=3000 }
 };
 
 
@@ -141,75 +132,68 @@ static const paramStruct tParam[2] =
  */
 static void FreeRTOS_Error(const portCHAR* msg)
 {
-    if ( NULL != msg )
-    {
-        vDirectPrintMsg(msg);
-    }
+	if ( NULL != msg ) {
+		vDirectPrintMsg(msg);
+	}
 
-    for ( ; ; );
+	for ( ; ; );
 }
 
 /* Startup function that creates and runs two FreeRTOS tasks */
 void main(void)
 {
-    /* Init of print related tasks: */
-    if ( pdFAIL == printInit(PRINT_UART_NR) )
-    {
-        FreeRTOS_Error("Initialization of print failed\r\n");
-    }
+	/* Init of print related tasks: */
+	if ( pdFAIL == printInit(PRINT_UART_NR) ) {
+		FreeRTOS_Error("Initialization of print failed\r\n");
+	}
 
-    /*
-     * I M P O R T A N T :
-     * Make sure (in startup.s) that main is entered in Supervisor mode.
-     * When vTaskStartScheduler launches the first task, it will switch
-     * to System mode and enable interrupt exceptions.
-     */
-    vDirectPrintMsg("= = = T E S T   S T A R T E D = = =\r\n\r\n");
+	/*
+	 * I M P O R T A N T :
+	 * Make sure (in startup.s) that main is entered in Supervisor mode.
+	 * When vTaskStartScheduler launches the first task, it will switch
+	 * to System mode and enable interrupt exceptions.
+	 */
+	vDirectPrintMsg("= = = T E S T   S T A R T E D = = =\r\n\r\n");
 
-    /* Init of receiver related tasks: */
-    if ( pdFAIL == recvInit(RECV_UART_NR) )
-    {
-        FreeRTOS_Error("Initialization of receiver failed\r\n");
-    }
+	/* Init of receiver related tasks: */
+	if ( pdFAIL == recvInit(RECV_UART_NR) ) {
+		FreeRTOS_Error("Initialization of receiver failed\r\n");
+	}
 
-    /* Create a print gate keeper task: */
-    if ( pdPASS != xTaskCreate(printGateKeeperTask, "gk", 128, NULL,
-                               PRIOR_PRINT_GATEKEEPR, NULL) )
-    {
+	/* Create a print gate keeper task: */
+	if ( pdPASS != xTaskCreate(printGateKeeperTask, "gk", 128, NULL,
+			PRIOR_PRINT_GATEKEEPR, NULL) ) {
         FreeRTOS_Error("Could not create a print gate keeper task\r\n");
-    }
+	}
 
-    if ( pdPASS != xTaskCreate(recvTask, "recv", 128, NULL, PRIOR_RECEIVER, NULL) )
-    {
-        FreeRTOS_Error("Could not create a receiver task\r\n");
-    }
+	if ( pdPASS != xTaskCreate(recvTask, "recv", 128, NULL, PRIOR_RECEIVER, NULL) ) {
+		FreeRTOS_Error("Could not create a receiver task\r\n");
+	}
 
-    /* And finally create two tasks: */
-    if ( pdPASS != xTaskCreate(vTaskFunction, "task1", 128, (void*) &tParam[0],
-                               PRIOR_PERIODIC, NULL) )
-    {
-        FreeRTOS_Error("Could not create task1\r\n");
-    }
+	/* And finally create two tasks: */
+	if ( pdPASS != xTaskCreate(vTaskFunction, "task1", 128, (void*) &tParam[0],
+			PRIOR_PERIODIC, NULL) ) {
+		FreeRTOS_Error("Could not create task1\r\n");
+	}
 
-    if ( pdPASS != xTaskCreate(vPeriodicTaskFunction, "task2", 128, (void*) &tParam[1],
-                               PRIOR_FIX_FREQ_PERIODIC, NULL) )
-    {
-        FreeRTOS_Error("Could not create task2\r\n");
-    }
+	if ( pdPASS != xTaskCreate(vPeriodicTaskFunction, "task2", 128, (void*) &tParam[1],
+			PRIOR_FIX_FREQ_PERIODIC, NULL) ) {
+		FreeRTOS_Error("Could not create task2\r\n");
+	}
 
-    vDirectPrintMsg("A text may be entered using a keyboard.\r\n");
-    vDirectPrintMsg("It will be displayed when 'Enter' is pressed.\r\n\r\n");
+	vDirectPrintMsg("A text may be entered using a keyboard.\r\n");
+	vDirectPrintMsg("It will be displayed when 'Enter' is pressed.\r\n\r\n");
 
-    /* Start the FreeRTOS scheduler */
-    vTaskStartScheduler();
+	/* Start the FreeRTOS scheduler */
+	vTaskStartScheduler();
 
-    /*
-     * If all goes well, vTaskStartScheduler should never return.
-     * If it does return, typically not enough heap memory is reserved.
-     */
+	/*
+	 * If all goes well, vTaskStartScheduler should never return.
+	 * If it does return, typically not enough heap memory is reserved.
+	 */
 
-    FreeRTOS_Error("Could not start the scheduler!!!\r\n");
+	FreeRTOS_Error("Could not start the scheduler!!!\r\n");
 
-    /* just in case if an infinite loop is somehow omitted in FreeRTOS_Error */
-    for ( ; ; );
+	/* just in case if an infinite loop is somehow omitted in FreeRTOS_Error */
+	for ( ; ; );
 }
